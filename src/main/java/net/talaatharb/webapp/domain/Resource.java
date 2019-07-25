6c8164f81a088e4cd724ca2880bd4765b9b95a97
mharb <mohamed.harb@cegedim.com>
2019-07-25 10:46:45 +0200

    Adding your first dependency: Lombok

--- src/main/java/net/talaatharb/webapp/domain/Resource.java
+++ src/main/java/net/talaatharb/webapp/domain/Resource.java
@@ -2,6 +2,9 @@ package net.talaatharb.webapp.domain;
 
 import java.util.List;
 
+import lombok.Data;
+
+@Data
 public class Resource {
 
 	/**
@@ -18,33 +21,4 @@ public class Resource {
 	 * A string to represent the name of the resource
 	 */
 	private String name;
-
-	public List<String> getData() {
-		return data;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setData(List<String> data) {
-		this.data = data;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	@Override
-	public String toString() {
-		return "Resource [id=" + id + ", name=" + name + ", data=" + data + "]";
-	}
 }
